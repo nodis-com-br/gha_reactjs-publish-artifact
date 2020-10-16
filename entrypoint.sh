3c5e742d2b0da52f97ae912a98c49aa30510e803
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-10-16 18:06:27 -0300

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,12 @@
 npm install react-scripts@3.3.0 -g
 npm install --only=prod
 npm install typescript
+
+source env-${NODIS_DEPLOY_ENV}.sh
 npm run build
 
 tar czvf ${NODIS_ARTIFACT_FILENAME} build
+
+aws s3 ls s3://${NODIS_ARTIFACT_BUCKET}/${NODIS_PROJECT_NAME}/
 
 aws s3 cp ${NODIS_ARTIFACT_FILENAME} s3://${NODIS_ARTIFACT_BUCKET}/${NODIS_PROJECT_NAME}/
