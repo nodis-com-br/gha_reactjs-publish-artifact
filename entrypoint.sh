bf6a47ee184d184e1268444c2d7fdc918d026e30
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-11-04 13:26:22 -0300

    treated missing env file error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ npm install react-scripts@3.3.0 -g
 npm install --only=prod
 npm install typescript
 
-source env-${NODIS_DEPLOY_ENV}.sh
+source env-${NODIS_DEPLOY_ENV}.sh || echo "env-${NODIS_DEPLOY_ENV}.sh not found"
 npm run build
 
 tar czvf ${NODIS_ARTIFACT_FILENAME} build
 
-aws --debug s3 cp ${NODIS_ARTIFACT_FILENAME} "s3://${NODIS_ARTIFACT_BUCKET}/${NODIS_PROJECT_NAME}/"
+aws s3 cp ${NODIS_ARTIFACT_FILENAME} "s3://${NODIS_ARTIFACT_BUCKET}/${NODIS_PROJECT_NAME}/"
